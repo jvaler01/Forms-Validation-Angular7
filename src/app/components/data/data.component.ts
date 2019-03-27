d9c745efdf0c4f890bc5236a96dec6a1f799fa41
Jorge Valero <jorgevm13@gmail.com>
2019-03-27 18:51:51 +0100

    Form validator data end

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {reject} from 'q';
 
 @Component({
   selector: 'app-data',
@@ -20,15 +22,34 @@ export class DataComponent implements OnInit {
     this.forma = new FormGroup({
       nombrecompleto: new FormGroup({
         nombre: new FormControl('', [Validators.required, Validators.minLength(3)]),
-        apellido: new FormControl('', Validators.required),
+        apellido: new FormControl('', [Validators.required,
+                                  this.noTermino]),
       }),
       correo: new FormControl('', [Validators.required,
                                 Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
       pasatiempos: new FormArray([
         new FormControl('Correr', Validators.required)
-      ])
+      ]),
+      username: new FormControl('', Validators.required, this.existeUsuario),
+      password1: new FormControl('', Validators.required),
+      password2: new FormControl(),
     });
     // this.forma.setValue(this.usuario);
+    this.forma.controls['password2'].setValidators([
+      Validators.required,
+      this.noIgual.bind( this.forma )
+    ]);
+    this.forma.valueChanges.subscribe( data => {
+      //console.log(data);
+    });
+
+    this.forma.controls['username'].valueChanges.subscribe( data =>{
+      console.log(data);
+    });
+
+    this.forma.controls['username'].statusChanges.subscribe( data =>{
+      console.log(data);
+    });
   }
 
   ngOnInit() {
@@ -41,5 +62,38 @@ export class DataComponent implements OnInit {
     (this.forma.controls.pasatiempos as FormArray).push(
       new FormControl('', Validators.required)
     );
+  }
+
+  noTermino( control: FormControl): {[s: string]: boolean} {
+    if ( control.value === 'asdf') {
+      return {
+        notermino: true
+      };
+    }
+    return null;
+  }
+
+  noIgual( control: FormControl): any {
+    let forma: any = this;
+    if ( control.value !== forma.controls['password1'].value) {
+      return {
+        noiguales: true
+      };
+    }
+    return null;
+  }
+
+  existeUsuario( control: FormControl): Promise<any>|Observable<any> {
+    let promesa = new Promise(
+      (resolve, reject ) => {
+        setTimeout( ()=>{
+          if ( control.value === 'pipo') {
+            resolve( {existe: true})
+          }else {
+            resolve( null )
+          }
+        }, 3000);
+      });
+    return promesa;
   }
 }
